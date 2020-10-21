a5b4c1d516021f7de0f1234f868ff95cbac5cb32
Jaydeep Shil <ED8A@eurofins.com>
2020-10-21 21:04:46 +0530

    Add project files.

---
+++ Sum of Two Integers without arithmetic operator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Sum_of_Two_Integers_without_arithmetic_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(GetSum(-3,12));
+        }
+
+        static int GetSum(int a, int b)
+        {
+            int carry;
+            while (b != 0)
+            {
+                carry = a & b;
+                a = a ^ b;
+                b = (carry) << 1;
+            }
+            return a;
+        }
+    }
+}
